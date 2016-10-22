488c26089f65bd81eac143b5dd2103964613b6a3
Kara Fulgum <karafulgum@gmail.com>
2016-10-22 10:36:31 -0500

    JS Setup

--- app/index.js
+++ app/index.js
@@ -1,1 +1,5 @@
 import 'whatwg-fetch';
+
+const form = document.querySelector('.search-form');
+const input = document.querySelector('.search-form__input');
+const result = document.querySelector('.results-list');
