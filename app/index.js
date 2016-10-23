60755153766e1ddd387b27e46caf71b7d1e5f675
Kara Fulgum <karafulgum@gmail.com>
2016-10-22 22:31:44 -0500

    Hooking up starting values

--- app/index.js
+++ app/index.js
@@ -1,5 +1,9 @@
 import 'whatwg-fetch';
+import data from './data';
+import NetflixResultView from './views/netflix-result';
 
-const form = document.querySelector('.search-form');
-const input = document.querySelector('.search-form__input');
-const result = document.querySelector('.results-list');
+const searchForm = document.querySelector('.search-form');
+const searchInput = document.querySelector('.search-form__input');
+const searchResult = document.querySelector('.results-list');
+
+const searchView = new NetflixResultView(searchResult, {});
